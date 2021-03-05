fc94bd0db7a6542ad6e2f2b2693b01097f3fa253
Linda See <seewenli@gmail.com>
2021-03-05 21:59:46 +0800

    deletion function

--- src/app/components/data-entry-form.component.ts
+++ src/app/components/data-entry-form.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AngularFireStorage } from '@angular/fire/storage';
 import { AngularFireAuth } from '@angular/fire/auth';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Observable } from 'rxjs';
 import { SampleForm } from '../models/form';
 import { BackendService } from '../services/backend.service';
@@ -18,7 +18,7 @@ export class DataEntryFormComponent implements OnInit {
   dataEntryForm: FormGroup
   uploadPercent: Observable<number>
   downloadURL: Observable<string>
-  image_urls: string[] = []
+  filePathArray: string[] = []
 
   constructor(private fb: FormBuilder, private bkSvc: BackendService, private afstore: AngularFireStorage, private afAuth: AngularFireAuth) { }
 
@@ -26,9 +26,18 @@ export class DataEntryFormComponent implements OnInit {
     this.dataEntryForm = this.fb.group({
       source_doc: ['', Validators.required],
       title: ['', Validators.required],
-      description: ['', Validators.required]
+      description: ['', Validators.required],
+      image_urls: this.fb.array([])
     })
     this.afAuth.signInAnonymously()
+  }
+
+  get image_urls() {
+    return this.dataEntryForm.get('image_urls') as FormArray
+  }
+
+  addImage_urls(image_url: string) {
+    this.image_urls.push(this.fb.control(image_url))
   }
 
   // login() {
@@ -45,31 +54,42 @@ export class DataEntryFormComponent implements OnInit {
     const filePath = '/test/' + file.name
     const ref = this.afstore.ref(filePath)
     const task = ref.put(file)
+    this.filePathArray.push(filePath)
     this.uploadPercent = task.percentageChanges()
     task.snapshotChanges().pipe(
       finalize(() => {
         this.downloadURL = ref.getDownloadURL()
-        console.log('ur',this.downloadURL)
-        this.downloadURL.subscribe(some => {
-          console.dir(some)
-          this.image_urls.push(some)
+        // console.log('ur',this.downloadURL)
+        this.downloadURL.subscribe(singleURL => {
+          console.dir(singleURL)
+          this.addImage_urls(singleURL)
+          console.log('images', this.image_urls.value)
         })
-        // this.image_urls.push()
-        console.log('images', this.image_urls)
       })
     )
     .subscribe()
   }
 
+  delete(i: number) {
+    console.log('before deletion', this.filePathArray)
+    console.log(this.image_urls.value)
+    const ref = this.filePathArray.splice(i, 1)
+    this.image_urls.removeAt(i)
+    this.afstore.ref(ref[0]).delete().subscribe(() => console.log('delete successful'))
+    console.log('after deletion', this.filePathArray)
+    console.log(this.image_urls.value)
+    
+  }
+
   submit() {
     const formData: SampleForm = this.dataEntryForm.value as SampleForm
     console.log(formData)
-    this.bkSvc.postNewRecord(formData)
-      .then(result => {
-        console.info('return from backend: ', result)
-        this.reset()
-      })
-      .catch(err => console.error(err))
+    // this.bkSvc.postNewRecord(formData)
+    //   .then(result => {
+    //     console.info('return from backend: ', result)
+    //     this.reset()
+    //   })
+    //   .catch(err => console.error(err))
   }
 
   reset() {
--- src/app/models/form.ts
+++ src/app/models/form.ts
@@ -2,4 +2,5 @@ export interface SampleForm {
     source_doc: string
     title: string
     description: string
+    image_urls: string []
 }
